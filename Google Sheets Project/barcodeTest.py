9744b11758a39f75346a277bc028e9cfecd64492
Carlos Sanchez <sabad.sanchez@gmail.com>
2023-02-18 17:44:19 -0800

    Working Barcode maker test,

--- Google Sheets Project/barcodeTest.py
+++ Google Sheets Project/barcodeTest.py
@@ -1,37 +1,70 @@
-from io import BytesIO
 import os
-
+from io import BytesIO
 from pathlib import Path
 
-from barcode import EAN13
 import barcode
 from barcode.writer import SVGWriter
 from barcode.writer import ImageWriter
 
-fileName = 'test'
-fileDirectory = 'Files/Programming/Python/python-barcode'
-filePath = Path.home() / fileDirectory/fileName
+fileName = 'default'
+fileDirectory = Path.home() / 'Barcode Output' 
+filePath = Path(fileDirectory)
 
-barcodes = [
-    ['009300000307', 'Mount Olive Sweet Relish'],
-    ['9800001088', 'tic tac cherry'],
-    ['9800007608', 'tic tac fruit'],
-    ['9800007615', 'tic tac freshmint'],
-    ['9800007639', 'tic tac orange'],
-    ['9800007677', 'tic tac wintergreen'],
-    ['9800123018', 'Ferrero Rocher'],
+if not filePath.exists():
+    os.makedirs(filePath)
+
+upca = barcode.get_barcode_class('upca')
+ean8 = barcode.get_barcode_class('ean8')
+EAN = barcode.get_barcode_class('ean13')
+
+barcode = None
+
+beerList = [
+['798373126020', 'Magic Hat 6 bottles', None , 'BEER' ],
+['798449008113', 'Ace Cider Pineapple 6pk bottle', None , 'BEER'] ,
+['798449008243', 'Ace Cider Pineapple 6pk cans',None , 'BEER' ],
+['810058730206', 'Flying Ember Cherry 6pk can', 'Hibiscus - kombucha', 'BEER' ],
+['810058730305', 'Flying Ember 6pk can', 'kombucha tropical hop', 'BEER' ],
+['01810622', 'Bud Light 12oz 6pk cans', None, 'BEER' ],
+['01811524', 'Bud Light 16oz 6pk cans', '6pk cans', 'BEER'] ,
+['01812620', 'Budweiser 40oz bottle',None , 'BEER' ],
+['01813027', 'King Cobra 32oz bottle', None, 'BEER' ] ,
 ]
 
-for i in barcodes:
-	print(i)
+prefix = 'upc_'
+for item in beerList:
+
+    if(len(item[0]) == 12):
+        fileName = prefix + str(item[0])
+        barcode = upca(item[0])
+
+    elif(len(item[0]) == 8):
+        fileName = prefix + str(item[0])
+        barcode = ean8(item[0])
+
+    else:
+        print('Unable to process: ' + i)
+        barcode = None
+
+
+    if barcode:
+        result = barcode.save(filePath / fileName)
+        print(fileName.ljust(16) + " was saved to : " + str(fileDirectory))
+
+
+
+	#print( str(len(i[0])))
 
     #rv = BytesIO()
     #EAN13(str(9300000307), writer=ImageWriter()).write(rv)
 
-barcode.PROVIDED_BARCODES
+#print(barcode.PROVIDED_BARCODES)
 
-EAN = barcode.get_barcode_class('ean13')
 
-my_ean = EAN(barcodes[0][0])
 
-result = my_ean.save(filePath)
+#my_ean = EAN(barcodes[0][0])
+
+
+#print(Path.cwd())
+#result = my_ean.save(filePath / fileName)
+#print("Your barcode was saved to : " + str(fileDirectory))
